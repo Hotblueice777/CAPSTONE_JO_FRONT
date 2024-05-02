118a53b164847e2af98e5f94ea701d09f9a1ca60
Hotblueice777 <joeyqi777@gmail.com>
2024-05-02 02:48:38 -0600

    first time commit component financialstatus_00

---
+++ src/components/FinancialStatus_00/FinancialStatus_00.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from "react-router-dom";
+import "../FinancialStatus_00/FinancialStatus_00.scss";
+
+function InputExpense() {
+
+    return(
+        <>
+        
+        </>
+    );
+}
+
+export default FinancialStatus_00
